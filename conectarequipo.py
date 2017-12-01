5e6d137e3eb7ccadbcc7f28dbfc36992864e0877
paulpy <pauljourdanbaez@gmail.com>
2017-12-01 10:22:58 -0300

    mejora en variables

--- conectarequipo.py
+++ conectarequipo.py
@@ -1,12 +1,25 @@
-#!usr/bin/env python
-#paramiko
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+Module to handle SSH Connections - Paramiko
+"""
 
 import paramiko
 
-def cumplicomando(comando):    
+C_SERVER = '192.168.1.56'
+C_PORT = 22
+C_USER = 'root'
+C_PASS = 'DEStruccion93'
+
+def cumplicomando(comando):
+    """
+    cumplicomando
+    Connect throught SSH and get a shell prompt from a host
+    Arguments: str: comando
+    """
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    ssh.connect('192.168.1.56',port=22,username='root',password='DEStruccion93')
-    stdin,stdout,stderr = ssh.exec_command(str(comando))
+    ssh.connect(C_SERVER, port=C_PORT, username=C_USER, password=C_PASS)
+    stdin, stdout, stderr = ssh.exec_command(str(comando))
     output = stdout.readlines()
     print "\n".join(output)
