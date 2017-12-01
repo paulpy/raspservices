bdb4b2324c872696943631d6028e2dd1661472de
paulpy <pauljourdanbaez@gmail.com>
2017-11-30 23:06:09 -0300

    def
    Agregando funcion y cabecera

--- conectarequipo.py
+++ conectarequipo.py
@@ -1,8 +1,12 @@
+#!usr/bin/env python
+#paramiko
+
 import paramiko
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-ssh.connect('192.168.1.56',port=22,username='root',password='DEStruccion93')
-stdin,stdout,stderr = ssh.exec_command('reboot')
-output = stdout.readlines()
-print "\n".join(output)
+def cumplicomando(comando):    
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    ssh.connect('192.168.1.56',port=22,username='root',password='DEStruccion93')
+    stdin,stdout,stderr = ssh.exec_command(str(comando))
+    output = stdout.readlines()
+    print "\n".join(output)
