6554061c52d65787fd84271d06cb7b76311c1ea5
paulpy <pauljourdanbaez@gmail.com>
2017-12-02 15:56:22 -0300

    cambios en lecturaestados

--- raspadmin/lecturaestados.py
+++ raspadmin/lecturaestados.py
@@ -35,13 +35,14 @@ def equipoconectado(con, ipequipo):
 
 def lecturaestadoequipo(con):
     historico = selectbd.selectultimohee(con)
-    estadoactual = historico.fetchone()
-    if estadoactual[3]=="2":
-        comandogpio.encender()
-        registrarenbd.historicoequipo("3","6",con)
-    if estadoactual[3]=="6":
-        diferencia=diferenciatiempo.diferenciadehora(estadoactual[1],datetime.datetime.now)
-        if diferencia > 200:
-            registrarenbd.historicoequipo("3","4",con)
-        else:
-            pass
+    estadoactuales = historico.fetchone()
+	for estadoactual in estadoactuales:	
+		if estadoactual[3]=="2":
+			comandogpio.encender()
+			registrarenbd.historicoequipo("3","6",con)
+		if estadoactual[3]=="6":
+			diferencia=diferenciatiempo.diferenciadehora(estadoactual[1],datetime.datetime.now)
+			if diferencia > 200:
+				registrarenbd.historicoequipo("3","4",con)
+			else:
+				pass
