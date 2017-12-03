a1962b8464c9b8bbd966694ce7b0913c85e55f12
paulpy <pauljourdanbaez@gmail.com>
2017-12-03 20:39:41 -0300

    agregango GPIO

--- raspadmin/comandogpio.py
+++ raspadmin/comandogpio.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 #comandogpio
+import RPi.GPIO as GPIO
+import time
+import lograspadmin
 
 def encender():
-    pass
+    lograspadmin.escribirlog("Encender equipo timepo 2 Segundos")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwaings(False)
+    GPIO.setup(17,GPIO.HIGH)
+    time.sleep(2)
+    GPIO.setup(17,GPIO.LOW)
 
 def apagar():
-    pass
+    lograspadmin.escribirlog("Apagar equipo timepo 5 Segundos")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwaings(False)
+    GPIO.setup(17,GPIO.HIGH)
+    time.sleep(5)
+    GPIO.setup(17,GPIO.LOW)
