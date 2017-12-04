98cec387cb2d6df741581b5d0f2c4aaf2840bc04
paulpy <pauljourdanbaez@gmail.com>
2017-12-04 15:15:08 -0300

    str add

--- registrarenhistorico.py
+++ registrarenhistorico.py
@@ -8,12 +8,12 @@ def historicoraspberry(causa, estado, con):
 	cursor = con.cursor()
 	escrituralog.escribirlog("Insercion en historicoraspberry")
 	fechahora = datetime.datetime.now()
-	cursor.execute("INSERT INTO raspapp_historico_estado_raspberry (fecha_hora_equipo_raspberry, causa_raspberry_fk_id, estado_raspberry_fk_id, raspberry_asignado_fk_id) VALUES (\'"+str(fechahora)+"\' ,"+causa+","+estado+",1)")
+	cursor.execute("INSERT INTO raspapp_historico_estado_raspberry (fecha_hora_equipo_raspberry, causa_raspberry_fk_id, estado_raspberry_fk_id, raspberry_asignado_fk_id) VALUES (\'"+str(fechahora)+"\' ,"+str(causa)+","+str(estado)+",1)")
 	con.commit()
 
 def historicoequipo(causa, estado, con):
 	cursor = con.cursor()
 	escrituralog.escribirlog("Insercion en historicoequipo")
 	fechahora = datetime.datetime.now()
-	cursor.execute("INSERT INTO raspapp_historico_estado_equipo (fecha_hora_equipo_estado, causa_equipo_fk_id, estado_equipo_fk_id, equipo_asignado_fk_id) VALUES (\'"+str(fechahora)+"\' ,"+causa+","+estado+",1)")
+	cursor.execute("INSERT INTO raspapp_historico_estado_equipo (fecha_hora_equipo_estado, causa_equipo_fk_id, estado_equipo_fk_id, equipo_asignado_fk_id) VALUES (\'"+str(fechahora)+"\' ,"+str(causa)+","+str(estado)+",1)")
 	con.commit()
