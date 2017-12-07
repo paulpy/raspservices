d73e88bba4f6c1bd90a3f4165196e941655ed599
pjourdan <pauljourdanbaez@gmail.com>
2017-12-07 11:23:18 -0300

    ejemplos de codico del log y de archivos de configuracion

--- testloggin.py
+++ testloggin.py
@@ -1,6 +1,21 @@
-import logging
-logging.basicConfig(filename='testlog.log',level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
+import logging as log
+import ConfigParser
+import os
+
+FORMAT_LOG = '%(asctime)s : %(levelname)s : %(message)s'
+FILE_NAME = 'loginiciosistema.log'
+LEVEL_F = log.INFO
+ÇONFIG_FILE_DIR = os.getcwd()
+CONFIG_FILE_NAME = '/config.ini'
+
+log.basicConfig(level=LEVEL_F, format=FORMAT_LOG, filename=FILE_NAME)
+
+user_input = raw_input('Ingrese palabra para el log: ')
+
+log.info('Prueba de log')
+log.info("Prueba de log")
+log.info('Palabra: %r', user_input)
+
+config = ConfigParser.ConfigParser()
+config = ConfigParser(CONFIG_FILE_DIR + Con)
+DEFAULT_PORT = config.get('main','default_app_port')
