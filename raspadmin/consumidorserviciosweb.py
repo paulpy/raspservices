fe83f9a6bcff709b4ebc19eb682c7a9e5df1777c
paulpy <pauljourdanbaez@gmail.com>
2017-12-19 14:15:20 -0300

    Mejoras RaspAdmin

--- raspadmin/consumidorserviciosweb.py
+++ raspadmin/consumidorserviciosweb.py
@@ -2,13 +2,11 @@
 #consumidorserviciosweb
 # -*- coding: UTF-8 -*-
 
-import sqlite3
 import selectbd
 import urlservices
 import coleccion
 import requests
 import logging as log
-import lograspadmin
 import registrarenbd
 
 FORMAT_LOG = '%(asctime)s : %(levelname)s : %(message)s'
@@ -27,15 +25,15 @@ def enviarlograsp(ipservidor, con):
         log.info('No Existe serie en la base de Datos Central')
     elif str(ultimoid) == 0:
         log.info('Sincronizando por Primerar Vez')
-        envio = coleccion.coleccion(selectbd.selecthistoricoraspultid(ultimoid,con))
+        envio = coleccion.coleccion(selectbd.selecthistoricoraspultid(ultimoid, con))
         rce = requests.post(urlservices.urlsincrohistoricorasp(ipservidor), json=envio)
     else:
         log.info('Corroborando Fila nula')
-        select = selectbd.selecthistoricoraspultid(ultimoid,con)
+        select = selectbd.selecthistoricoraspultid(ultimoid, con)
         filanula = select.fetchall()
         if filanula != None:
             log.info('Enviando Datos para actualizar')
-            envio = coleccion.coleccion(selectbd.selecthistoricoraspultid(ultimoid,con))
+            envio = coleccion.coleccion(selectbd.selecthistoricoraspultid(ultimoid, con))
             rce = requests.post(urlservices.urlsincrohistoricorasp(ipservidor), json=envio)
         else:
             log.info('No es necesario sincronizar')
@@ -45,22 +43,22 @@ def enviarlogequi(ipservidor, con):
     log.info('*****Metodo enviar log del equipo*****')
     select = selectbd.selectidentificadorequi(con)
     for serie in select:
-    	url = urlservices.urlhistoricoequi(ipservidor,str(serie[0]))
+    	url = urlservices.urlhistoricoequi(ipservidor, str(serie[0]))
     rc = requests.get(url)
     ultimoid = rc.json()
     if str(ultimoid) == -1:
-    	log.info('No Existe serie en la base de Datos Central')
+        log.info('No Existe serie en la base de Datos Central')
     elif str(ultimoid) == 0:
-    	log.info('Sincronizando por Primerar Vez')
-    	envio = coleccion.coleccion(selectbd.selecthistoricoequiultid(ultimoid,con))
-    	rce = requests.post(urlservices.urlsincrohistoricoequi(ipservidor), json=envio)
+        log.info('Sincronizando por Primerar Vez')
+        envio = coleccion.coleccion(selectbd.selecthistoricoequiultid(ultimoid, con))
+        rce = requests.post(urlservices.urlsincrohistoricoequi(ipservidor), json=envio)
     else:
         log.info('Corroborando Fila nula')
-        select = selectbd.selecthistoricoraspultid(ultimoid,con)
+        select = selectbd.selecthistoricoraspultid(ultimoid, con)
         filanula = select.fetchall()
         if filanula != None:
             log.info('Enviando Datos para actualizar')
-            envio = coleccion.coleccion(selectbd.selecthistoricoequiultid(ultimoid,con))
+            envio = coleccion.coleccion(selectbd.selecthistoricoequiultid(ultimoid, con))
             rce = requests.post(urlservices.urlsincrohistoricoequi(ipservidor), json=envio)
         else:
             log.info('No es necesario sincronizar')
@@ -76,12 +74,12 @@ def interaccionws(ipservidor, con):
             log.info('No trajo interacciones')
         else:
             log.info('Se encontraron interacciones')
-            registrarenbd.interacciones(con,value["comando"],value["idAccion"],str(value["idInteraccion"]),str(value["codInteraccion"]))
-    log.info('****Fin del Metodo*****')
+            registrarenbd.interacciones(con, value["comando"], value["idAccion"], str(value["idInteraccion"]), str(value["codInteraccion"]))
+    log.info('*****Fin del Metodo*****')
 
 def actualizarinteraccion(ipservidor, con, id):
     log.info('*****Metodo enviar actualizacion de interaccion cumplida*****')
-    cursor = selectbd.selectactualizarinte(con,id)
+    cursor = selectbd.selectactualizarinte(con, id)
     envioactualizar = cursor.fetchall()
     rfh = requests.post(urlservices.urlactualizarinteraccion(ipservidor), json=coleccion.coleccionenviointeraccion(envioactualizar))
-    log.info('****Fin del metodo*****')
+    log.info('*****Fin del metodo*****')
--- raspadmin/raspadmin.py
+++ raspadmin/raspadmin.py
@@ -4,7 +4,6 @@
 import sqlite3
 import time
 import logging as log
-import lograspadmin
 import registrarenbd
 import pingserver
 import consumidorserviciosweb
@@ -17,20 +16,20 @@ LEVEL_F = log.INFO
 log.basicConfig(level=LEVEL_F, format=FORMAT_LOG, filename=FILE_NAME)
 
 con = sqlite3.connect("/root/raspproyect/raspweb/db.sqlite3")
-ipservidor = "192.168.1.54"
+ipservidor = "192.168.0.200"
 ipequipo = "192.168.0.51"
 log.info('*****----Inicio del RaspAdmin----*****')
 registrarenbd.historicoraspberry(6, 1, con)
 con.close()
 def raspprocess():
     con = sqlite3.connect("/root/raspproyect/raspweb/db.sqlite3")
-    lecturaestados.equipoconectado(con,ipequipo)
+    lecturaestados.equipoconectado(con, ipequipo)
     if pingserver.isAlive(ipservidor):
         log.info('*****Existe conxion con el servidor*****')
-        consumidorserviciosweb.enviarlograsp(ipservidor,con)
-        consumidorserviciosweb.enviarlogequi(ipservidor,con)
-        consumidorserviciosweb.interaccionws(ipservidor,con)
-        cumplimientoaccion.realizarinteraccionespendientes(ipservidor,con)
+        consumidorserviciosweb.enviarlograsp(ipservidor, con)
+        consumidorserviciosweb.enviarlogequi(ipservidor, con)
+        consumidorserviciosweb.interaccionws(ipservidor, con)
+        cumplimientoaccion.realizarinteraccionespendientes(ipservidor, con)
     else:
         log.info('*****No existe conxion con el servidor*****')
     lecturaestados.lecturaestadoequipo(con)
